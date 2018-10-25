5eb60d41b02e1f4c44a59106b2ad8e524c5645e9
Ali Sanaknaki <asana085@uottawa.ca>
2018-10-25 12:39:59 -0400

    Pushing for finn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js';
+import App from './v1/App.js';
+
 import * as serviceWorker from './serviceWorker';
 
 // import Home from './components/Home';
